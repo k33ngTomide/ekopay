d5511c89a28d7998c98e2d71ea7df5c999942be1
Muiliyu Sodiq Akintomide <msodiqakintomide@gmail.com>
2025-06-29 23:21:31 +0100

    cors fixes

--- ekopay-backend/src/main.ts
+++ ekopay-backend/src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    // origin: process.env.CORS_ORIGIN || '*',
+    origin: '*',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
